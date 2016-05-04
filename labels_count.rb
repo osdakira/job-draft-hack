4323c97920b11a83a7ff00752c16b5019f13f7a3
Akira Osada <osd.akira@gmail.com>
2016-05-04 21:54:13 +0900

    ラベルの0-1表を作成

---
+++ labels_count.rb
@@ -0,0 +1,15 @@
+# Label の [0, 1] 表を作る
+require "csv"
+load "./models.rb"
+
+labels_count = Label.count
+CSV.open("tmp.csv", "w") do |csv|
+  csv << Label.all.pluck(:name)
+  User.includes(:users_labels).each do |user|
+    labels = Array.new(labels_count, 0)
+    user.users_labels.map(&:label_id).each do |label_id|
+      labels[label_id - 1] = 1
+    end
+    csv << labels
+  end
+end
